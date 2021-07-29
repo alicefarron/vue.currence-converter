09afa4e0c886c370059a1e5a62351bf2bf38dc8e
alicefarron <alicefarron@ya.ru>
2021-07-29 22:54:43 +0500

    Добавила таблицу

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <table class="table tablesorter">
+    <thead>
+      <tr>
+        <slot name="columns">
+          <th v-for="column in columns" :key="column"> {{ column.name }}</th>
+        </slot>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <slot :row="item">
+          <td v-for="(column, index) in columns"
+              :key="index"
+              >
+            {{itemValue(item, column.value)}}
+          </td>
+        </slot>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+ export default {
+   name: 'base-table',
+   props:{
+     data:{
+       type: Array,
+       default: () => []
+     },
+     columns:{
+       type: Array,
+       default: () => []
+     }
+   },
+   methods: {
+     hasValue(item, column) {
+       return item[column.toLowerCase()] !== "undefined";
+     },
+     itemValue(item, column) {
+       return item[column.toLowerCase()];
+     }
+   }
+ }
+</script>
+<style>
+.table {
+    width: 100%;
+}
+
+thead {
+    border: 1px solid #818181;
+}
+
+td {
+    border: 1px solid #818181;
+    padding: 10px;
+}
+</style>
--- src/pages/MainScreen.vue
+++ src/pages/MainScreen.vue
@@ -3,23 +3,39 @@
     <div class="main-page__container">
       <section class="base-block">
         <h3 class="base-block__title">Конвертация валют</h3>
-        <div>
-          <div>
-            <select class="select-country" @change="updateInputs" v-model="countrySelect">
+        <div class="base-block__field">
+          <div class="country-choose">
+            <select class="country-choose__select" @change="updateInputs" v-model="countrySelectFirst">
               <option v-for="country in countries" :key="country">
                 <p>{{country}}</p>
               </option>
             </select>
-            <input v-model="firstVal" />
+            <input class="country-choose__field" v-model="firstVal" />
+          </div>
+          <button class="separator-button" title="Переключить валюту">
+            <img src="../assets/img/transfer-arrows.svg" />
+          </button>
+          <div class="country-choose">
+            <select class="country-choose__select" @change="updateInputs" v-model="countrySelectSecond">
+              <option v-for="country in countries" :key="country">
+                <p>{{country}}</p>
+              </option>
+            </select>
+            <input class="country-choose__field" v-model="firstVal" />
           </div>
         </div>
       </section>
+
       <section class="base-block">
-        <h3 class="base-block__title">Конвертация валют</h3>
+        <h3 class="base-block__title">Список стоимостей</h3>
+        
+        <base-table :data="priceTable.data" :columns="priceTable.columns">
+        </base-table>
 
       </section>
       <section class="base-block">
-        <h3 class="base-block__title">Конвертация валют</h3>
+
+        <h3 class="base-block__title">Список истории курса конвертации</h3>
 
       </section>
     </div>
@@ -29,17 +45,48 @@
 
 <script>
 import axios from 'axios';
+import BaseTable from '../components/BaseTable.vue';
 
   export default {
+    components: {
+      BaseTable,
+    },
     data() {
       return {
         firstVal: 0,
         secondVal: 0,
-        countrySelect: [],
+        countrySelectFirst: '',
+        countrySelectSecond: '',
         countries: [
           'USA',
           'RUS',
-        ]
+        ],
+        priceTable: {
+          columns: [{
+            name: 'Стоимость',
+            value: 'price'
+          },
+          {
+            name: 'USD -> EUR',
+            value: 'USD'
+          },
+          {
+            name: 'EUR -> USD',
+            value: 'EUR'
+          }],
+          data: [
+            {
+              price: 1,
+              USD: '345',
+              EUR: '123',
+            }, 
+            {
+              price: 10,
+              USD: '533',
+              EUR: '433',
+            }
+          ]
+        },
       };
     },
     methods: {
@@ -65,9 +112,19 @@ body {
   margin: 0;
 }
 
+button {
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    opacity: .6;
+  }
+}
+
 .main-page {
   min-height: calc(100vh - 128px);
   padding: 30px;
+  background-color: rgb(245, 245, 245);
 
   &__container {
     display: flex;
@@ -76,10 +133,44 @@ body {
 
 .base-block {
   width: 30%;
-  padding: 20px;
+  padding: 40px 20px;
+  background-color: #fff;
+  border-radius: 10px;
+
+  &:not(:last-child) {
+    margin-right: 2%;
+  }
 
   &__title {
     font-size: 1.3rem;
+    margin-top: 0;
+  }
+
+  &__field {
+    display: flex;
+  }
+}
+
+.country-choose {
+  &__select {
+    border-radius: 5px;
+    padding: 6px 10px;
+  }
+
+  &__field {
+    padding: 6px 10px;
+    border: 1px solid #bebebe;
+    border-radius: 5px;
+    margin-top: 10px;
+  }
+}
+
+.separator-button {
+  cursor: pointer;
+
+  img {
+    width: 20px;
+    height: 20px;
   }
 }
 </style>
